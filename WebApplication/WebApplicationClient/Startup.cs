26c32fd569db829b1ab6efd9fa96b5e7933bf523
roymanish1982 <roymanish1982@gmail.com>
2017-12-04 17:39:54 +0530

    sfrsdfsdfsdf
    zscxfzxczxc zxc

---
+++ WebApplication/WebApplicationClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationClient.Startup))]
+namespace WebApplicationClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
